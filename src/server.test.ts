6a4d7dfdf147a57e6592287e2ce1946f90625cac
gene poletski <8095687-gene_poletski@users.noreply.gitlab.com>
2021-08-01 22:49:52 +1200

    Add create shipping list item basic functionality

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,28 +1,73 @@
 import Client from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
 import { Server } from './server';
 
+interface ShoppingListItem {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
+class ShoppingListService {
+  private items: ShoppingListItem[];
+
+  constructor() {
+    this.items = [];
+  }
+
+  public save(itemInfo: {
+    title: string;
+    completed: boolean;
+  }): Promise<ShoppingListItem> {
+    const item = {
+      id: uuidv4(),
+      title: itemInfo.title,
+      completed: itemInfo.completed,
+    };
+
+    this.items.push(item);
+
+    return Promise.resolve(item);
+  }
+
+  public findById(id: string): Promise<ShoppingListItem> {
+    return Promise.resolve(this.items.find((item) => item.id === id));
+  }
+}
+
 describe('Shopping list management', () => {
-  describe('Shopping list items CRUD', () => {
-    let server, clientSocket;
+  let server, clientSocket, shoppingListService;
 
-    beforeEach((done) => {
-      const port = 3000;
-      server = new Server({ port });
-      server.start(() => {
-        clientSocket = Client(`http://localhost:${port}`);
-        done();
-      });
-    });
-
-    afterEach((done) => {
-      clientSocket.close();
-      server.stop();
+  beforeEach((done) => {
+    const port = 3000;
+    shoppingListService = new ShoppingListService();
+    server = new Server({ port, shoppingListService });
+    server.start(() => {
+      clientSocket = Client(`http://localhost:${port}`);
       done();
     });
+  });
 
-    it('Create a shopping list item', (done) => {
-      clientSocket.on('hello', (arg) => {
-        expect(arg).toBe('world');
+  afterEach((done) => {
+    clientSocket.close();
+    server.stop();
+    done();
+  });
+
+  describe('Create a shopping list item', () => {
+    it('should create an item entity', (done) => {
+      const entityInfo = {
+        title: 'lorem ipsum',
+        completed: false,
+      };
+
+      clientSocket.emit('shoppingListItem:create', entityInfo, async (res) => {
+        const storedEntity = await shoppingListService.findById(res.payload.id);
+        expect(storedEntity).toMatchObject({
+          id: res.payload.id,
+          title: entityInfo.title,
+          completed: entityInfo.completed,
+        });
         done();
       });
     });
--- src/server.ts
+++ src/server.ts
@@ -3,26 +3,40 @@ import { Server as IOServer, Socket } from 'socket.io';
 
 interface ServerConfig {
   port: number;
+  shoppingListService: any;
 }
 
 export class Server {
   private httpServer: HttpServer;
   private ioServer: IOServer;
+  private shoppingListService: any;
   private port: number;
 
-  constructor({ port }: ServerConfig) {
+  constructor({ port, shoppingListService }: ServerConfig) {
     this.port = port;
     this.httpServer = createServer();
     this.ioServer = new IOServer(this.httpServer, {
       //
     });
+    this.shoppingListService = shoppingListService;
     this.setupIOServer();
   }
 
   private setupIOServer(): void {
     this.ioServer.on('connection', (socket: Socket) => {
-      socket.emit('hello', 'world');
-      // ToDo! Find if socket close required
+      socket.on('shoppingListItem:create', async (payload: any, cb) => {
+        const saved = await this.shoppingListService.save({
+          title: payload.title,
+          completed: payload.completed,
+        });
+        cb({
+          payload: {
+            id: saved.id,
+            title: saved.title,
+            completed: saved.completed,
+          },
+        });
+      });
     });
   }
 
