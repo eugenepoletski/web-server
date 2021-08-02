415fbeb92a6bca2bb0a86af37fa5263e3633d4c1
gene poletski <8095687-gene_poletski@users.noreply.gitlab.com>
2021-08-02 20:31:09 +1200

    Refactor app start

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import { start } from './server';
+import { Server } from './server';
+import { ShoppingListService } from './services/shoppingList';
 
-start({ port: 3000 });
+const server = new Server({
+  port: 3000,
+  shoppingListService: new ShoppingListService(),
+});
+
+server.start(() => {
+  console.log('Server listening on port 3000');
+});
